3f4d0b143c19c9dd18bfc7f4d05e6f5206771a3c
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-05-17 09:24:27 +0200

    add GenerateRandomParams function

--- GestForm/Program.cs
+++ GestForm/Program.cs
@@ -13,7 +13,7 @@ public class Program
         public int Count;
     }
     
-    private static List<IDivisibilityRule> rules = new()
+    private static readonly List<IDivisibilityRule> rules = new()
     {
         new DivisibleByThreeAndFiveRule(),
         new DivisibleByThreeRule(),
@@ -26,22 +26,24 @@ public class Program
 
     public static void Main()
     {
-        // Création des paramètres de nombres aléatoires
-        var randomParam = new RandomParams
-        {
-            LowerLimit = -1000,
-            UpperLimit = 1000,
-            Count = 50
-        };
-        
-        // Générer des nombres aléatoires
-        var numbers = GenerateRandomNumbers(randomParam);
+        var randomParams = GenerateRandomParams(-1000, 1000, 50);
+        var numbers = GenerateRandomNumbers(randomParams);
 
         // Afficher le message pour chaque nombre
         foreach (var number in numbers)
         {
             Console.WriteLine(GetMessageForNumber(number));
         }
+    }
+
+    public static RandomParams GenerateRandomParams(int lowerLimit, int upperLimit, int count)
+    {
+        return new RandomParams
+        {
+            LowerLimit = lowerLimit,
+            UpperLimit = upperLimit,
+            Count = count
+        };
     }
 
     public static IEnumerable<int> GenerateRandomNumbers(RandomParams randomParams)
